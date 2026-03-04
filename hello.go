3eaf81d97e247039f1967ce575fcd5e11d925256
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2026-03-04 12:44:58 -0300

    Refactor main function to include user interaction and display program details

--- hello.go
+++ hello.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Meu primeiro programa em Go!")
+	var nome string = "Gabriel"
+	var idade int = 26
+	var versao float32 = 1.26
+	fmt.Println("Olá, sr. " + nome)
+	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("Olá, sr", nome, "sua idade é", idade, "anos")
+
+	fmt.Printf("O tipo de variavel versão é %T\n", versao)
+
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	fmt.Scanf("%d", &comando)
+
+	fmt.Println("O comando escolhido foi", comando)
+
 }
