5917c5b80af9fd83e0203bf480e08fdf096ae053
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2026-03-04 16:53:43 -0300

    Refactor iniciarMonitoramento to improve site testing logic and streamline output

--- hello.go
+++ hello.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
@@ -54,6 +58,7 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
@@ -71,11 +76,20 @@ func iniciarMonitoramento() {
 
 	fmt.Println(sites)
 
-	for i, site := range sites {
-		fmt.Println("Estou passando na posição", i, "do meu slice e essa posição tem o site", site)
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
 	}
 
-	site := "https://random-status-code.herokuapp.com/"
+	fmt.Println("")
+
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
