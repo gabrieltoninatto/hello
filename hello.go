ba17c73ae9947a7856c935a66ed738f7bd4a4da0
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2026-03-04 23:02:55 -0300

    Add log printing functionality and timestamp to log entries

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -30,6 +31,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Print("Saindo do programa")
 			os.Exit(0)
@@ -148,6 +150,18 @@ func registraLog(site string, status bool) {
 		fmt.Println("Ocorreu um erro:", err)
 	}
 
-	arquivo.WriteString(site + " - online: " + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
